780fc37c189a763029c0c9e1a6a70badae2b5d63
Edward Medvedev <edward.medvedev@gmail.com>
2016-10-23 19:53:58 +0200

    Moving to root

--- src/client/app/index.jsx
+++
@@ -1,8 +0,0 @@
-import 'normalize-css/normalize.css';
-import 'vis/dist/vis.min.css';
-import '../themes/main.css';
-
-import {render} from 'react-dom';
-import Visualizer from './Visualizer.jsx';
-
-render(<Visualizer/>, document.getElementById('visualizer'));
